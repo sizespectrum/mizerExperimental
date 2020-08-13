cc1ebf79a0b6ee7f3f96e258bfefcb8c18f502ca
Gustav Delius <gustav.delius@gmail.com>
2020-08-13 09:18:49 +0100

    `compareParams()` now returns the result rather than printing it.

--- R/compareParams.R
+++ R/compareParams.R
@@ -18,26 +18,31 @@ compareParams <- function(params1, params2) {
     assert_that(is(params1, "MizerParams"))
     assert_that(is(params2, "MizerParams"))
 
+    result <- character()
+
     # species parameters ----
     sp_names1 <- names(params1@species_params)
     sp_names2 <- names(params2@species_params)
-    sp_diff1 <- setdiff(sp_names2, sp_names1)
+    sp_diff1 <- setdiff(sp_names1, sp_names2)
     if (length(sp_diff1) > 0) {
-        print(paste("params1 has the following additional species parameters:",
-                    toString(sp_diff1)))
+        msg <- paste("params1 has the following additional species parameters:",
+                     toString(sp_diff1))
+        result <- c(result, msg)
     }
-    sp_diff2 <- setdiff(sp_names1, sp_names2)
+    sp_diff2 <- setdiff(sp_names2, sp_names1)
     if (length(sp_diff2) > 0) {
-        print(paste("params2 has the following additional species parameters:",
-                    toString(sp_diff2)))
+        msg <- paste("params2 has the following additional species parameters:",
+                     toString(sp_diff2))
+        result <- c(result, msg)
     }
     sp_names <- intersect(sp_names1, sp_names2)
 
     sp_eq <- all.equal(params1@species_params[, sp_names],
                        params2@species_params[, sp_names])
     if (!isTRUE(sp_eq)) {
-        print(paste("The following species parameters differ:",
-              toString(sp_eq)))
+        msg <- paste("The following species parameters differ:",
+               toString(sp_eq))
+        result <- c(result, msg)
         # ctable <- compareDF::compare_df(params1@species_params[, sp_names],
         #                       params2@species_params[, sp_names],
         #                       keep_unchanged_cols = FALSE)
@@ -51,18 +56,19 @@ compareParams <- function(params1, params2) {
     res2 <- res2[order(names(res2))]
     res_eq <- all.equal(res1, res2)
     if (!isTRUE(res_eq)) {
-        print(paste("The following resource parameters differ:",
-                    toString(res_eq)))
+        msg <- paste("The following resource parameters differ:",
+                     toString(res_eq))
+        result <- c(result, msg)
     }
 
     # size grid ----
     if (length(params1@w) != length(params2@w)) {
-        print("The community size bins differ.")
-        return(invisible())
+        msg <- "The community size bins differ."
+        result <- c(result, msg)
     }
     if (length(params1@w_full) != length(params2@w_full)) {
-        print("The resource size bins differ.")
-        return(invisible())
+        msg <- "The resource size bins differ."
+        result <- c(result, msg)
     }
 
     # other slots ----
@@ -72,9 +78,10 @@ compareParams <- function(params1, params2) {
         }
         eq <- all.equal(slot(params1, sl), slot(params2, sl))
         if (!isTRUE(eq)) {
-            print(paste("The", sl, "slots do not agree:",
-                        toString(eq)))
+            msg <- paste("The", sl, "slots do not agree:",
+                         toString(eq))
+            result <- c(result, msg)
         }
     }
-    invisible()
+    result
 }
