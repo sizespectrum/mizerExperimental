bdebb40b88b171df29a7c19b053396ddecbe4950
Gustav Delius <gustav.delius@gmail.com>
2020-08-19 12:02:55 +0100

    Temporarily adding functions that are so far only in the development version of mizer. Can be removed after the release of mizer 2.0.4

--- tests/testthat/test-projectToSteady.R
+++ tests/testthat/test-projectToSteady.R
@@ -51,7 +51,8 @@ test_that("steady works", {
     p_mizer <- mizer::steady(params, t_per = 2)
     expect_identical(p, p_mizer)
     # and works the same when returning sim
-    sim <- steady(params, t_per = 2, return_sim = TRUE)
-    sim_mizer <- mizer::steady(params, t_per = 2, return_sim = TRUE)
-    expect_identical(sim, sim_mizer)
+    skip("Waiting for mizer 2.0.4")
+    # sim <- steady(params, t_per = 2, return_sim = TRUE)
+    # sim_mizer <- mizer::steady(params, t_per = 2, return_sim = TRUE)
+    # expect_identical(sim, sim_mizer)
 })
