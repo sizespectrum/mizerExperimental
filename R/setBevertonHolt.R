e0065adea8da2656713a7f53ab6da7f6c2cb8345
Gustav Delius <gustav.delius@gmail.com>
2021-07-05 08:22:55 +0100

    Some improvements in man page for `setBevertonHolt()`

--- R/setBevertonHolt.R
+++ R/setBevertonHolt.R
@@ -1,13 +1,17 @@
 #' Set Beverton-Holt density dependence
 #'
 #' Takes a MizerParams object `params` with arbitrary density dependence and
-#' returns a MizerParams object with Beverton-Holt density-dependence. There is
-#' a one-parameter family of Beverton-Holt reproduction curves that have the
-#' property that, for the initial size spectrum for each species, the energy
-#' invested into reproduction leads to the reproduction rate that is required to
-#' maintain the given egg abundance. By specifying one of the parameters
-#' `erepro`, `R_max` or `reproduction_level` you pick the desired reproduction
-#' curve. More details of these parameters are provided below.
+#' returns a MizerParams object with Beverton-Holt density-dependence with your
+#' chosen reproductive efficiency or your chosen maximal reproduction rate.
+#' If you have tuned your `params` object to describe a particular steady state,
+#' then setting the Beverton-Holt density dependence with this function will
+#' leave you with the exact same steady state, because it only allows you to
+#' choose among those Beverton-Holt reproduction curves that have the property
+#' that the energy invested into reproduction by the mature individuals leads to
+#' the reproduction rate that is required to maintain the given egg abundance.
+#' By specifying one of the parameters `erepro`, `R_max` or `reproduction_level`
+#' you pick the desired reproduction curve. More details of these parameters are
+#' provided below.
 #'
 #' With Beverton-Holt density dependence the relation between the energy
 #' invested into reproduction and the number of eggs hatched is determined
@@ -21,10 +25,10 @@
 #' \deqn{R_{di} = \frac{\text{erepro}}{2 w_{min}} E_R,}{R_di = (erepro/(2 w_min)) E_R,}
 #' where the proportionality factor is given by the reproductive efficiency
 #' `erepro` divided by the egg size `w_min` to convert energy to egg number and
-#' by a factor of 2 to account for the two sexes.
+#' divided by 2 to account for the two sexes.
 #'
 #' Imposing a finite maximum reproduction rate \eqn{R_{max}}{R_max} leads to a
-#' non-linear relationship between energy invested and eggs hatchted. This
+#' non-linear relationship between energy invested and eggs hatched. This
 #' density-dependent reproduction rate \eqn{R_{dd}}{R_dd} is given as
 #' \deqn{R_{dd} = R_{di}
 #' \frac{R_{max}}{R_{di} + R_{max}}.}{R_dd = R_di R_max/(R_di + R_max).}
@@ -69,30 +73,36 @@
 #'
 #' This plot shows that a given energy \eqn{E_R} invested into reproduction can
 #' lead to the same reproduction rate \eqn{R_{dd}}{R_dd} with different choices
-#' of the parameters `R_max` and `erepro`. A higher `R_max` coupled with a lower
-#' `erepro` can give the same value as a lower `R_max` coupled with a higher
-#' `erepro`.
-#'
-#' We denote the ratio between the density-dependent reproduction rate
-#' \eqn{R_{dd}}{R_dd} and the maximal reproduction rate  \eqn{R_{max}}{R_max}
-#' the `reproduction_level`.
+#' of the parameters `R_max` and `erepro`. `R_max` determines the asymptote of
+#' the curve and `erepro` its initial slope. A higher `R_max` coupled with a
+#' lower `erepro` (black curves) can give the same value as a lower `R_max`
+#' coupled with a higher `erepro` (blue curves).
 #'
 #' For the given initial state in the MizerParams object `params` one can
 #' calculate the energy \eqn{E_R} that is invested into reproduction by the
-#' mature individuals. One can also calculate the reproduction rate
-#' \eqn{R_{dd}}{R_dd} that is required to keep the egg abundance constant. This
-#' determines the location of the black dot in the above graph. You then only
-#' need one parameter to select one from the family of Beverton-Holt
-#' curves going through that point. This parameter can be `erepro`, `R_max` or
-#' the `reproduction_level`.
+#' mature individuals and the reproduction rate \eqn{R_{dd}}{R_dd} that is
+#' required to keep the egg abundance constant. These two values determine the
+#' location of the black dot in the above graph. You then only need one
+#' parameter to select one curve from the family of Beverton-Holt curves going
+#' through that point. This parameter can be `erepro` or `R_max`. Instead of
+#' `R_max` you can alternatively specify the `reproduction_level` which is the
+#' ratio between the density-dependent reproduction rate \eqn{R_{dd}}{R_dd} and
+#' the maximal reproduction rate  \eqn{R_{max}}{R_max}.
 #'
 #' The parameter you provide can be either a vector, with one value for each
 #' species, or a single value that is then used for all species. The
 #' values for `R_max` must be larger than \eqn{R_{dd}}{R_dd} and can range
 #' up to `Inf`. The values for the `reproduction_level` must be positive and
 #' less than 1. The values for `erepro` must be large enough to allow the
-#' required reproducion rate. They should also be smaller than 1 to be
+#' required reproduction rate. They should also be smaller than 1 to be
 #' physiologically sensible, but this is not enforced by the function.
+#'
+#' As can be seen in the graph above, choosing a lower value for `R_max` or a
+#' higher value for `erepro` means that near the steady state the reproduction
+#' will be less sensitive to a change in the energy invested into reproduction
+#' and hence less sensitive to changes in the spawning stock biomass or its
+#' energy income. As a result the species will also be less sensitive to
+#' fishing, leading to a higher F_MSY.
 #'
 #' @param params A MizerParams object
 #' @param erepro Reproductive efficiency for each species. See details.
@@ -100,8 +110,8 @@
 #' @param reproduction_level Sets at which proportion of the maximum
 #'   reproduction rate the actual reproduction takes place in the initial state.
 #'   See details.
-#' @param R_factor `r lifecycle::badge("deprecated")` Use `reproduction_level`
-#'   instead, with `R_factor = 1 / reproduction_level`.
+#' @param R_factor `r lifecycle::badge("deprecated")` Use
+#'   `reproduction_level = 1 / R_factor` instead.
 #'
 #' @return A MizerParams object
 #' @export
