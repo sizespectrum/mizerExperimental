b270c397383a98baf78698267c9314085de7b77a
Gustav Delius <gustav.delius@gmail.com>
2021-08-18 14:51:14 +0100

    New function `matchBiomasses()` extracted from `tuneParams()`. Still need to write documentation and actually use it in `tuneParams()`.

---
+++ R/matchBiomasses.R
@@ -0,0 +1,36 @@
+#' Match biomasses to observations
+#' 
+#' @param params A MizerParams object
+#' @inheritParams valid_species_arg
+#' @return A MizerParams object
+#' @export
+#' @examples 
+#' params <- NS_params
+#' species_params(params)$biomass_observed <- 
+#'     c(0.8, 61, 12, 35, 1.6, 20, 10, 7.6, 135, 60, 30, 78)
+#' species_params(params)$biomass_cutoff <- 10
+#' params <- calibrateBiomass(params)
+#' params <- matchBiomass(params)
+#' plotBiomassVsSpecies(params)
+matchBiomasses <- function(params, species = NULL) {
+    if (!("biomass_observed" %in% names(params@species_params))) {
+        return(params)
+    }
+    species <- valid_species_arg(params, species = species, 
+                                 return.logical = TRUE) &
+        !is.na(params@species_params$biomass_observed) &
+        params@species_params$biomass_observed > 0
+    for (sp in (1:nrow(params@species_params))[species]) {
+        cutoff <- params@species_params$biomass_cutoff[[sp]]
+        if (is.null(cutoff) || is.na(cutoff)) {
+            cutoff <- params@species_params$w_mat[[sp]] / 20
+        }
+        total <- sum((params@initial_n[sp, ] * params@w * params@dw)
+                     [params@w >= cutoff])
+        n0_old <- params@initial_n[sp, params@w_min_idx[[sp]]]
+        n0 <- n0_old * params@species_params$biomass_observed[[sp]] / total
+        # rescale abundance to new egg density
+        params@initial_n[sp, ] <- params@initial_n[sp, ] * n0 / n0_old
+    }
+    params
+}
---
+++ tests/testthat/test-matchBiomasses.R
@@ -0,0 +1,25 @@
+test_that("matchBiomasses works", {
+    params <- NS_params
+    # Does nothing when no observed biomass
+    expect_identical(matchBiomasses(params), params)
+    species_params(params)$biomass_observed <- NA
+    expect_identical(matchBiomasses(params), params)
+    # Does nothing if observed already equals model
+    species_params(params)$biomass_cutoff <- 1e-4
+    species_params(params)$biomass_observed <- 
+        rowSums(sweep(params@initial_n, 2, params@w * params@dw, "*"))
+    expect_equal(matchBiomasses(params), params)
+    # Even if only partially observed
+    species_params(params)$biomass_observed[1:5] <- NA
+    expect_equal(matchBiomasses(params), params)
+    # If we double the observations, we get twice the abundance
+    species <- 1:9
+    species_params(params)$biomass_observed <- 
+        species_params(params)$biomass_observed * 2
+    params2 <- matchBiomasses(params, species)
+    expect_equal(params2@initial_n[6:9, ], params@initial_n[6:9, ] * 2)
+    # but unobserved species don't change
+    expect_equal(params2@initial_n[1:5, ], params@initial_n[1:5, ])
+    # and unselected species don't change
+    expect_equal(params2@initial_n[10:12, ], params@initial_n[10:12, ])
+})
