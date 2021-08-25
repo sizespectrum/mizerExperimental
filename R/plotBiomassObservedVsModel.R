47f717ec1e4263d7b59fe074dca150970df991c2
Samik Datta <samik.datta@niwa.co.nz>
2021-08-25 18:20:33 +1200

    Updated plot function: combined Pearson and fraction into one.

--- R/plotBiomassObservedVsModel.R
+++ R/plotBiomassObservedVsModel.R
@@ -1,57 +1,65 @@
 ## Functions for plotting simulated vs. observed biomass data
 
 #' @export
-plotBiomassObservedVsModelPearson = function(sim, log_scale = T) {
+plotBiomassObservedVsModel = function(sim, fraction = F, log_scale = T, species = NULL) {
   
-  if (log_scale == T) {
-    dummy = data.frame(sim@params@species_params$species, log10(getBiomass(sim)[nrow(sim@n), ]), log10(sim@params@species_params$biomass_observed))
-    xlab = 'log10(predicted biomass)' 
-    ylab = 'log10(observed biomass)'
-  } else {
-    dummy = data.frame(sim@params@species_params$species, getBiomass(sim)[nrow(sim@n), ], sim@params@species_params$biomass_observed)
-    xlab = 'predicted biomass' 
-    ylab = 'observed biomass'
+  # preliminary checks
+  assert_that(is(sim, "MizerSim"))
+  species = valid_species_arg(sim, species)
+  row_select = match(species, sim@params@species_params$species) # find rows corresponding to species selected
+  if (!"biomass_observed" %in% names(sim@params@species_params)) {
+    stop(paste0("You have not provided values for the column 'biomass_observed' in ", deparse(substitute(sim))), '@params@species_params.')
+  } else if (!is.numeric(sim@params@species_params$biomass_observed)) {
+    stop(paste0("The column 'biomass_observed' in ", deparse(substitute(sim))), '@params@species_params is not numeric, please fix.')
+  } else { # accept
+    biomass_observed = sim@params@species_params$biomass_observed
   }
-  names(dummy) = c('species', 'simulation', 'data')
-  res <- cor.test(dummy$simulation, dummy$data, method = "pearson")
-  pc = round(res$estimate, digits = 4)
-  dummy$species = factor(dummy$species, levels = dummy$species[order(dummy$data, decreasing = T)]) # order by decreasing species biomass in data
   
-  gg = ggplot(data = dummy, aes(x = data, y = simulation, colour = species, label = species))
-  gg = gg + geom_point(size = 3) +
+  # Build dataframe
+  dummy = data.frame(species, getBiomass(sim)[nrow(sim@n), row_select], biomass_observed[row_select]) # fraction of sim/data
+  names(dummy) = c('species', 'simulation', 'data')
+  dummy$species = factor(dummy$species, levels = dummy$species[order(dummy$data, decreasing = T)]) # order by decreasing species biomass in data
+  xlab = 'predicted biomass' 
+  ylab = 'observed biomass'
+  
+  if (fraction == F & log_scale == T) { # For Pearson only, allow log10 biomasses
+    dummy = dummy %>% mutate(simulation = log10(simulation), data = log10(data))
+    xlab = 'log10(predicted biomass)' # update axis labels
+    ylab = 'log10(observed biomass)'
+  } 
+  
+  # pull out NA biomasses
+  dummy = dummy %>% filter(!is.na(data))
+  
+  if (fraction == F) { # for Pearson
+    res <- cor.test(dummy$simulation, dummy$data, method = "pearson") # Pearson's correlation coefficient
+    pc = round(res$estimate, digits = 3) # rounded down to 3 digits
+    title = paste("Pearsons coefficient =", pc)
+    ylim = range(dummy %>% select(simulation, data)) # set y-limit
+  } else { # for fraction
+    res <- sum(abs(1 - dummy$simulation/dummy$data)) # sum of absolute differences
+    pc = round(res, digits = 3) # rounded down to 3 digits
+    dummy$simulation = dummy$simulation/dummy$data # replace simulation biomass with fraction
+    title = paste("Total difference =", pc)
+    ylim = range(dummy$simulation)
+  }
+  
+  gg = ggplot(data = dummy, aes(x = data, y = simulation, colour = species, label = species)) +
+    geom_point(size = 3) +
     geom_label_repel(box.padding   = 0.35,
                      point.padding = 0.5,
                      segment.color = 'grey50', 
                      show.legend = F,
                      max.overlaps = Inf) +
-    geom_abline(aes(intercept = 0, slope = 1), colour = 'purple', linetype = "dashed", size = 1.3) +
-    coord_cartesian(ylim = (range(dummy %>% select(simulation, data)))) +
-    labs(x = xlab, y = ylab, title = paste("Pearsons coefficient = ", pc), 
-         color = "Legend")
+    coord_cartesian(ylim = ylim) +
+    labs(x = xlab, y = ylab, title = title, color = "Legend")
+  
+  if (fraction == F) {
+    gg = gg + geom_abline(aes(intercept = 0, slope = 1), colour = 'purple', linetype = "dashed", size = 1.3) # y = x line
+  } else {
+    gg = gg + geom_hline(aes(yintercept = 1), linetype = "dashed", colour = 'purple', size = 1.3)
+  }
+  
   gg # output
 }
 
-#' @export
-plotBiomassObservedVsModelFraction = function(sim) {
-  dummy = data.frame(sim@params@species_params$species, getBiomass(sim)[nrow(sim@n), ], sim@params@species_params$biomass_observed) # fraction of sim/data
-  names(dummy) = c('species', 'simulation', 'data')
-  res <- sum(abs(1 - dummy$simulation/dummy$data))
-  pc = round(res, digits = 3)
-  dummy$fraction = dummy$simulation/dummy$data
-  dummy$species = factor(dummy$species, levels = dummy$species[order(dummy$data, decreasing = T)]) # order by decreasing species biomass in data
-  
-  gg = ggplot(data = dummy, aes(x = data, y = fraction, colour = species, label = species))
-  gg = gg + geom_point(size = 3) +
-    geom_label_repel(box.padding   = 0.35,
-                     point.padding = 0.5,
-                     segment.color = 'grey50', 
-                     show.legend = F,
-                     max.overlaps = Inf) +
-    # geom_text(show.legend = F) +
-    # geom_text(hjust = 0, vjust = 0) +
-    geom_hline(aes(yintercept = 1), linetype = "dashed", colour = 'purple', size = 1.3) +
-    labs(x = 'Biomass from data', y = 'Fraction of biomass in simulation', title = paste("Total difference = ", pc), color = "Legend")
-  
-  gg # output
-  
-}
