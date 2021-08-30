e4586c492d22e18d8371e73e55d679c583f8eec9
Samik Datta <samik.datta@niwa.co.nz>
2021-08-31 10:20:11 +1200

    Move test to tests/testthat/test-plotBiomassObservedVsModel.R

--- tests/testthat/test-plotBiomassObservedVsModel.R
+++ tests/testthat/test-plotBiomassObservedVsModel.R
@@ -1,29 +1,56 @@
+## Get test up and running for plotBiomassObservedVsModel.R
+
+rm(list = ls())
+setwd("C:/Users/dattas/Documents/mizerExperimental/R") # set directory if necessary
+
+library(testthat)
+library(mizerExperimental)
+library(tidyverse)
+
+source('plotBiomassObservedVsModel.R')
+
 test_that("plotBiomassObservedVsModel works", {
-  
-  # Set up parameters
-  params <- NS_params
-  species_params(params)$biomass_observed <-
-    c(0.8, 61, 12, 35, 1.6, 20, 10, 7.6, 135, 60, 30, 78)
-  species_params(params)$biomass_cutoff <- 10
-  params <- calibrateBiomass(params)
-  dummy = plotBiomassObservedVsModel(params, return_data = T) # get data frame out
-  
-  # Check biomasses equal those put in
-  expect_equal(dummy$observed, species_params(params)$biomass_observed) # check observed_biomasses are equal
-  
-  # Try removing observed biomasses, check it still checks out
-  params2 = params # copy over
-  species_params(params2)$biomass_observed[c(1, 7, 10)] = NA
-  dummy = plotBiomassObservedVsModel(params2, return_data = T) # get data frame out
-  expect_equal(dummy$is_observed, !is.na(species_params(params2)$biomass_observed)) # check that dummy has correct observations
-  expect_equal(dummy$observed[dummy$is_observed], species_params(params2)$biomass_observed[!is.na(species_params(params2)$biomass_observed)]) # check observed_biomasses are equal
-  
-  # # Try removing species, check it still checks out
-  sp_select = c(1, 4, 7, 10, 11, 12) # choose some species
-  dummy = plotBiomassObservedVsModel(params, species = sp_select, return_data = T) # get reduced data frame out
-  expect_equal(nrow(dummy), length(sp_select)) # check that dummy is right size
-  expect_equal(dummy$observed, species_params(params)$biomass_observed[sp_select]) # check observed_biomasses are equal
-  
-  # Finally, look at plot
-  
+
+# Set up parameters
+params <- NS_params
+
+# check you get error without biomass_observed column
+expect_error(plotBiomassObservedVsModel(params))
+
+# pull out data frame for biomass comparison
+species_params(params)$biomass_observed <-
+  c(0.8, 61, 12, 35, 1.6, 20, 10, 7.6, 135, 60, 30, 78)
+species_params(params)$biomass_cutoff <- 10
+params <- calibrateBiomass(params)
+dummy = plotBiomassObservedVsModel(params, return_data = T) # get data frame out
+
+# Check biomasses equal those put in
+expect_equal(dummy$observed, species_params(params)$biomass_observed) # check observed_biomasses are equal
+
+# check that you get error with no species
+expect_error(plotBiomassObservedVsModel(params, species = rep(F, 12)))
+
+# Try removing observed biomasses, check it still checks out
+params2 = params # copy over
+species_params(params2)$biomass_observed[c(1, 7, 10)] = NA
+dummy = plotBiomassObservedVsModel(params2, return_data = T) # get data frame out
+expect_equal(dummy$is_observed, !is.na(species_params(params2)$biomass_observed)) # check that dummy has correct observations
+expect_equal(dummy$observed[dummy$is_observed], species_params(params2)$biomass_observed[!is.na(species_params(params2)$biomass_observed)]) # check observed_biomasses are equal
+
+# # Try removing species, check it still checks out
+sp_select = c(1, 4, 7, 10, 11, 12) # choose some species
+dummy = plotBiomassObservedVsModel(params, species = sp_select, return_data = T) # get reduced data frame out
+expect_equal(nrow(dummy), length(sp_select)) # check that dummy is right size
+expect_equal(dummy$observed, species_params(params)$biomass_observed[sp_select]) # check observed_biomasses are equal
+
+# Finally, look at plot
+dummy = plotBiomassObservedVsModel(params, return_data = T) # get data frame out
+p <- plotBiomassObservedVsModel(params) # get plot
+expect_true(is.ggplot(p))
+expect_identical(p$labels$x, "observed biomass")
+expect_identical(p$labels$y, "model biomass")
+expect_identical(p$data, dummy)
+
 })
+
+
