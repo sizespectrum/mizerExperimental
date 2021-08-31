7a6ba1e5a25a212e380d52d68e7822cbf015f885
Gustav Delius <gustav.delius@gmail.com>
2021-08-31 11:22:21 +0200

    Testing `show_unobserved` argument

--- tests/testthat/test-plotBiomassObservedVsModel.R
+++ tests/testthat/test-plotBiomassObservedVsModel.R
@@ -19,15 +19,21 @@ expect_equal(dummy$observed, species_params(params)$biomass_observed)
 # check that you get error with no species
 expect_error(plotBiomassObservedVsModel(params, species = rep(F, 12)))
 
-# Try removing observed biomasses, check it still checks out
+# Try removing observed biomasses.
 params2 = params # copy over
 species_params(params2)$biomass_observed[c(1, 7, 10)] = NA
-dummy = plotBiomassObservedVsModel(params2, return_data = T) # get data frame out
-expect_equal(dummy$is_observed, 
-             !is.na(species_params(params2)$biomass_observed))
-expect_equal(dummy$observed[dummy$is_observed], 
+# plot without unobserved species
+dummy = plotBiomassObservedVsModel(params2, return_data = T)
+expect_equal(as.character(dummy$species), 
+             species_params(params)$species[!is.na(species_params(params2)$biomass_observed)])
+expect_equal(dummy$observed, 
              species_params(params2)$biomass_observed
              [!is.na(species_params(params2)$biomass_observed)])
+# plot with unobserved species
+dummy = plotBiomassObservedVsModel(params2, return_data = T,
+                                   show_unobserved = TRUE)
+expect_equal(as.character(dummy$species), 
+             species_params(params)$species)
 
 # # Try removing species, check it still checks out
 sp_select = c(1, 4, 7, 10, 11, 12) # choose some species
