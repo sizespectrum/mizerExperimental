8e53ef3ef05b9aacc2d2bd3b6c2b32e0bba329d8
Romain Forestier <romain.forestier48@gmail.com>
2021-11-29 11:32:20 +1100

    resource spectrum is updated to its new steady state value

--- R/resourceControl.R
+++ R/resourceControl.R
@@ -19,7 +19,9 @@ resourceControl <- function(input, output, session, params, flags, ...) {
             mizerMR::resource_params(p)$w_max[sel_re]  <- input$w_pp_cutoff
             mizerMR::resource_params(p)$n[sel_re]      <- input$n_resource
             mizerMR::resource_params(p)$w_min[sel_re]  <- input$w_min_resource
-            mizerMR::initialNResource(p) <- mizerMR::resource_capacity(p)
+            mu <- getResourceMort(p)
+            mizerMR::initialNResource(p) <- mizerMR::resource_rate(p) *
+                mizerMR::resource_capacity(p) / (mizerMR::resource_rate(p) + mu)
         } else {
             p <- setResource(p,
                              kappa = input$kappa,
