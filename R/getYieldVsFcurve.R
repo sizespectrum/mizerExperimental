b3a45588929cc16672f6e2fabd9d188d078bd4ee
Romain Forestier <romain.forestier48@gmail.com>
2021-12-02 13:49:40 +1100

    converting duplicates of for loop into yieldCalculator function

--- R/getYieldVsFcurve.R
+++ R/getYieldVsFcurve.R
@@ -15,7 +15,7 @@
 #' @param F_max The maximum fishing mortality. Used only if `F_range` is missing.
 #' @param no_steps The number of steps to use. Only used if `F_range` is missing.
 #' @param effort_it The width of steps used to find maximum fisheries mortality,
-#' higher values means a faster calculation but less accuarate maximum
+#' higher values means a faster calculation but less accurate maximum.
 #'
 #' @return A data frame with columns `F` and `yield`.
 #' @export
@@ -30,9 +30,8 @@
 #' }
 getYieldVsF <- function(params,
                         species,
+                        F_range,
                         no_steps = 10,
-                        F_max = 3,
-                        F_range = seq(0, F_max, length.out = no_steps),
                         effort_it = 1) {
     # Check parameters
     params <- validParams(params)
@@ -44,7 +43,7 @@ getYieldVsF <- function(params,
     if (length(idx_species) != 1) {
         stop("Invalid species specification")
     }
-    assert_that(is.numeric(F_range))
+
     # First make a new gear for that specific species
     sp_name <- params@species_params$species[[idx_species]]
     gp <- gear_params(params)
@@ -61,147 +60,56 @@ getYieldVsF <- function(params,
     gear_params(params) <- rbind(gp, gp_extra)
     initial_effort(params)["tmp"] <- initial_effort(params)[gp$gear[gps]] # setting initial effort same as original gear
     effort <- getInitialEffort(params)
-    yield <- 0 * F_range
-    # Loop over fishing mortalities:
 
-    # Find where species biomass crashes using getMaxF()
-    maxFdf <- getMaxF(params = params, idx_species = idx_species, effort_it = effort_it)
-    # getMaxF already took a look at some yield values,
-    # need to complete to get a smoother curve
-    # need also to calculate values below initial F
-    # how many time steps do we want by the end?
-
-    # TODO check that simulations starts close to steady state already
-
-    if(no_steps > dim(maxFdf)[1]) # if the user asked for more steps than already calculated by getMaxF
+    if(missing(F_range))
     {
-        add_steps <- no_steps - dim(maxFdf)[1]
+        maxFdf <- getMaxF(params = params, idx_species = idx_species, effort_it = effort_it) # find maximum viable fisheries effort
 
-        # what we should get
-        opt_seq <- seq(0,max(maxFdf$effort), length.out = no_steps)
-
-        # remove values close to those we already have
-        dupes <- NULL
-        for(iEffort in maxFdf$effort)
+        if(no_steps > dim(maxFdf)[1]) # if the user asked for more steps than already calculated by getMaxF
         {
-# these duplicates are the seq_pos in opt_seq and their diff with maxFdf effort
-            # so basically no need to run these in opt_seq as we already have them!
-            dupes <- rbind(dupes,c(abs(opt_seq[which.min(abs(opt_seq - iEffort))]- iEffort),which.min(abs(opt_seq - iEffort))))
-        }
+            add_steps <- no_steps - dim(maxFdf)[1] # number of effort values to calculate
+            opt_seq <- seq(0,max(maxFdf$effort), length.out = no_steps) # what a uniform effort value vector looks like
+            # remove values close to those getMaxF already calculated
+            dupes <- NULL
+            for(iEffort in maxFdf$effort)
+                dupes <- rbind(dupes,c(abs(opt_seq[which.min(abs(opt_seq - iEffort))]- iEffort),
+                                       which.min(abs(opt_seq - iEffort))))
+
             dupes <- dupes[order(-dupes[,1]),]
+            # if need more effort values than already calculates, don't add in between getMaxF's efforts
+            if(add_steps > dim(maxFdf)[1])
+                effort_todo <- opt_seq[-dupes[,2]] else
+                    # if need to add a few efforts inbetween the one already calculated, add where their neighbors are furthest
+                    effort_todo <- opt_seq[dupes[1:add_steps,2]]
 
-            if(add_steps > dim(maxFdf)[1]) # if we need more steps than we have already calculated, don;t do the one close to maxFdf
-            {
-effort_todo <- opt_seq[-dupes[,2]]
-            } else {
+            # dividing effort vector between "before initial effort" and "after" to reach steady state faster
+            effort_vec <- effort_todo[order(effort_todo)] # order so steady states are close to each other
+            effort_down <- effort_vec[effort_vec < initial_effort(params)["tmp"]]
+            effort_down <- effort_down[order(effort_down,decreasing = TRUE)]
+            effort_up <- effort_vec[effort_vec > initial_effort(params)["tmp"]]
 
-            effort_todo <- opt_seq[dupes[1:add_steps,2]] # if we need to add a few more steps between the one we already have, add where there is the most space
-            }
+            yield_down <- yieldCalculator(params = params, effort_vec = effort_down, idx_species = idx_species)
+            yield_up <- yieldCalculator(params = params, effort_vec = effort_up, idx_species = idx_species)
 
-            effort_vec <- effort_todo[order(effort_todo)] # order to steady states are close to each other
+            yield_vec <- c(yield_down,yield_up)
+            effort_vec <- c(effort_down,effort_up)
 
+        } else { # already above maximum number of steps but need to calculate from initial F to 0
+            myEffort <- initial_effort(params)["tmp"] - effort_it
+            # if effort_it is too big, just need to calculate the 0 value
+            if(myEffort > 0) effort_vec <- seq(0,myEffort,by = effort_it) else effort_vec <- 0
 
-
-
-        yield_vec <- NULL
-        for(iEffort in effort_vec)
-        {
-
-
-            params@initial_effort["tmp"] <- iEffort
-            # print(iEffort)
-            sim <- projectToSteady(params, t_max = 100,
-                                   return_sim = TRUE, progress_bar = FALSE)
-            y <- getYield(sim)
-
-
-            ft <- idxFinalT(sim)
-            # print("number of time steps")
-            # print(ft)
-
-            if (ft < 66) { # if convergence use final yield
-                yield_vec <- c(yield_vec, y[ft, idx_species])
-            } else { # otherwise average over last 30 years (t_per = 1.5)
-                yield_vec <- c(yield_vec, mean(y[(ft - 20):ft, idx_species]))
-            }
-            # Start next simulation with final state from current in the hope that
-            # this will be quicker.
-            params <- setInitialValues(params, sim)
-            # print("biomass left")
-            # print(sum(params@initial_n[idx_species,which(params@w >= params@gear_params$knife_edge_size[idx_species])]))
-
+            yield_vec <- yieldCalculator(params = params, effort_vec = effort_vec, idx_species = idx_species)
 
         }
+        return(rbind(maxFdf,data.frame("yield" = yield_vec, "effort" = effort_vec)))
+    } else {
+        assert_that(is.numeric(F_range))
 
-    } else { # already above max resolution but need to calculate from inititial F to 0
+        yield_vec <- yieldCalculator(params = params, effort_vec = F_range, idx_species = idx_species)
 
-        myEffort <- initial_effort(params)["tmp"] - effort_it
-
-        if(myEffort > 0) effort_vec <- seq(0,myEffort,by = effort_it) else effort_vec <- 0 # if effort_it is too big, just need to calculate the 0 value
-
-
-        yield_vec <- NULL
-        for(iEffort in effort_vec)
-        {
-
-
-            params@initial_effort["tmp"] <- iEffort
-            # print(iEffort)
-
-            sim <- projectToSteady(params, t_max = 100,
-                                   return_sim = TRUE, progress_bar = FALSE)
-            y <- getYield(sim)
-
-
-            ft <- idxFinalT(sim)
-            # print("number of time steps")
-            # print(ft)
-
-            if (ft < 66) { # if convergence use final yield
-                yield_vec <- c(yield_vec, y[ft, idx_species])
-            } else { # otherwise average over last 30 years (t_per = 1.5)
-                yield_vec <- c(yield_vec, mean(y[(ft - 20):ft, idx_species]))
-            }
-            # Start next simulation with final state from current in the hope that
-            # this will be quicker.
-            params <- setInitialValues(params, sim)
-            # print("biomass left")
-            # print(sum(params@initial_n[idx_species,which(params@w >= params@gear_params$knife_edge_size[idx_species])]))
-
-
-        }
-
-
-
-
-
-
+        return(data.frame("yield" = yield_vec, "effort" = F_range))
     }
-    # add new yield values to maxFdf
-    newEffort <- data.frame("yield" = yield_vec, "effort" = effort_vec)
-    maxFdf <- rbind(maxFdf,newEffort)
-
-
-
-    # for (i in seq_along(F_range)) {
-    #     params@initial_effort["tmp"] <- F_range[i]
-    #     sim <- projectToSteady(params, t_max = 100,
-    #                            return_sim = TRUE, progress_bar = FALSE)
-    #     y <- getYield(sim)
-    #     ft <- idxFinalT(sim)
-    #
-    #     if (ft < 66) { # if convergence use final yield
-    #         yield[i] <- y[ft, idx_species]
-    #     } else { # otherwise average over last 30 years (t_per = 1.5)
-    #         yield[i] <- mean(y[(ft - 20):ft, idx_species])
-    #     }
-    #     # Start next simulation with final state from current in the hope that
-    #     # this will be quicker.
-    #     params <- setInitialValues(params, sim)
-    # }
-
-    # return(data.frame(F = F_range, yield = yield))
-    return(maxFdf)
 }
 
 #' Plot a yield-vs-F curve
@@ -223,14 +131,12 @@ effort_todo <- opt_seq[-dupes[,2]]
 plotYieldVsF <- function(params,
                          species,
                          no_steps = 10,
-                         F_max = 3,
-                         F_range = seq(0, F_max, length.out = no_steps)) {
+                         F_range){
 
     curve <- getYieldVsF(params,
                          species = species,
                          F_range = F_range,
-                         no_steps = no_steps,
-                         F_max = F_max)
+                         no_steps = no_steps)
 
     ggplot(curve, aes(x = effort, y = yield)) +
         geom_line() +
@@ -240,42 +146,32 @@ plotYieldVsF <- function(params,
 }
 
 
-
+#' Maximum viable fisheries effort for species
+#'
+#' @description
 #' Calculate the maximum fisheries effort before reaching a specific threshold
 #' Default case is biomass exposed to fisheries lower than 10% of original biomass
 #'
-#'@description
-#'TBD
-#'
-#' @param MizerParams
+#' @param params MizerParams
 #' @param idx_species species row number in `params@species_params`
-#' @param effort_it distance between each effort iteration
+#' @param effort_it The width of steps used to find maximum fisheries mortality,
+#' higher values means a faster calculation but less accurate maximum.
 #'
 #' @return dataframe
 #' @export
 getMaxF <- function(params, idx_species, effort_it = 1)
 {
-
-
     iEffort <- effort_init <- initial_effort(params)["tmp"]
-    # effort_it <- 1 # effort iteration value
+    # threshold to stop the while loop
     biom_threshold <- sum(params@initial_n[idx_species,which(params@w >= params@gear_params$knife_edge_size[idx_species])])/10
     yield_vec <- NULL
     while (sum(params@initial_n[idx_species,which(params@w >= params@gear_params$knife_edge_size[idx_species])]) >= biom_threshold)
     {
-
-
         params@initial_effort["tmp"] <- iEffort
-        # print(iEffort)
         sim <- projectToSteady(params, t_max = 100,
                                return_sim = TRUE, progress_bar = FALSE,distance_func = distanceSSLogYield, tol = .01)
         y <- getYield(sim)
-
-
         ft <- idxFinalT(sim)
-        # print("number of time steps")
-        # print(ft)
-
         if (ft < 66) { # if convergence use final yield
             yield_vec <- c(yield_vec, y[ft, idx_species])
         } else { # otherwise average over last 30 years (t_per = 1.5)
@@ -284,29 +180,20 @@ getMaxF <- function(params, idx_species, effort_it = 1)
         # Start next simulation with final state from current in the hope that
         # this will be quicker.
         params <- setInitialValues(params, sim)
-        # print("biomass left")
-        # print(sum(params@initial_n[idx_species,which(params@w >= params@gear_params$knife_edge_size[idx_species])]))
-
         iEffort <- iEffort + effort_it
     }
-
-    #max_F <- iEffort - effort_it # need to remove last iteration because of while loop
-
     res <- data.frame("yield" = yield_vec, "effort" = seq(effort_init,length(yield_vec), by = effort_it))
-
-    return(res) # last value of res$effort is max_F
+    # last value of res$effort is max_F
+    return(res)
 
 }
-
 
 #' Measure distance between current and previous state in terms of yield.
 #'
 #' @description
-#'
 #' Calculates the proportional difference between getYield() outputs of current
 #' and previous state. This function can be used in projectToSteady() to decide
 #' when sufficient convergence to steady state has been achieved.
-#'
 #'
 #' @param params MizerParams
 #' @param current A named list with entries `n`, `n_pp` and `n_other`
@@ -318,39 +205,28 @@ getMaxF <- function(params, idx_species, effort_it = 1)
 #' @family distance functions
 #' @export
 
-distanceSSLogYield <- function(params, current, previous, criterion = "proportion"){
-
+distanceSSLogYield <- function(params, current, previous, criterion = "proportion")
+{
     effort <- params@initial_effort
     time_range <- 0
     t <- min(time_range)
     yield <- list()
-
-    # print(params@w)
-    #
-    # print(summary(current))
-    # print(current$n)
-    # print(previous)
 
     for(sim_version in c("current","previous"))
     {
         switch (sim_version,
                 "current" = {
                     biomass <- sweep(current$n, 2, params@w * params@dw, "*")
-                    # biomass <- biomass[dim(biomass)[1],,]
-
                     n <- current$n
                     n_pp <- current$n_pp
                     n_other <- current$n_other},
                 "previous" = {
-                    biomass <- sweep(current$n, 2, params@w * params@dw, "*")
-                    # biomass <- biomass[dim(biomass)[1],,]
-
-                    n <- current$n
-                    n_pp <- current$n_pp
-                    n_other <- current$n_other},
+                    biomass <- sweep(previous$n, 2, params@w * params@dw, "*")
+                    n <- previous$n
+                    n_pp <- previous$n_pp
+                    n_other <- previous$n_other},
                 {}
         )
-
         no_gears <- dim(params@catchability)[[1]]
         f <- get(params@rates_funcs$FMort)
         if (length(dim(effort)) == 2) {
@@ -401,8 +277,44 @@ distanceSSLogYield <- function(params, current, previous, criterion = "proportio
 
     if(criterion == "SSE")
         res <- sum((log(yield$current[sel]) - log(yield$previous[sel]))^2) # SSLog
-    else if(criterion =="proportion")
+    else if(criterion == "proportion")
         res <- abs(sum(yield$current[sel] - yield$previous[sel])) / sum(yield$previous[sel]) # propotion change
 
     return(res)
+}
+
+
+#' Calculates yield from steady sim
+#'
+#' @description
+#' This function replaces a loop used multiple times within
+#' `getYieldVsCurve`
+#'
+#' @param params Mizer params object
+#' @param effort_vec vector of effort values to calcualte the yield from
+#' @param idx_species targeted species row number in `params@species_params`
+#'
+#' @return a vector of yield value of same length as `effort_vec`
+#'
+yieldCalculator <- function(params, effort_vec, idx_species)
+{
+    yield_vec <- NULL
+    for(iEffort in effort_vec)
+    {
+    params@initial_effort["tmp"] <- iEffort
+    sim <- projectToSteady(params, t_max = 100,
+                           return_sim = TRUE, progress_bar = FALSE,distance_func = distanceSSLogYield, tol = .01)
+    y <- getYield(sim)
+    ft <- idxFinalT(sim)
+    if (ft < 66) { # if convergence use final yield
+        yield_vec <- c(yield_vec, y[ft, idx_species])
+    } else { # otherwise average over last 30 years (t_per = 1.5)
+        yield_vec <- c(yield_vec, mean(y[(ft - 20):ft, idx_species]))
+    }
+    # Start next simulation with final state from current in the hope that
+    # this will be quicker.
+    params <- setInitialValues(params, sim)
+    }
+
+    return(yield_vec)
 }
