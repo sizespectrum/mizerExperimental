dead03ee84056ef314e5dd438c32f12bd3bf462b
Romain Forestier <romain.forestier48@gmail.com>
2022-02-18 15:54:31 +1100

    edit home page with figures + default effort set to 1 for SEAparams

--- R/homeTab.R
+++ R/homeTab.R
@@ -12,6 +12,26 @@
 homeTab <- function(input, output, session,
                        params, logs, trigger_update, ...) {
 
+    output$eu <- renderImage({
+        list(src = normalizePath(file.path('./www/EU.jpg')))
+    }, deleteFile = FALSE)
+#
+#     output$imas <- renderImage({
+#         list(src = normalizePath(file.path('./www/IMAS.jpg')))
+#     }, deleteFile = FALSE)
+#
+#     output$mizer <- renderImage({
+#         list(src = normalizePath(file.path('./www/mizer.jpg')))
+#     }, deleteFile = FALSE)
+#
+#     output$nature <- renderImage({
+#         list(src = normalizePath(file.path('./www/NatureResearchCentre.png')))
+#     }, deleteFile = FALSE)
+
+    output$banner <- renderImage({
+        list(src = normalizePath(file.path('./www/tuneParamsHome.png')))
+    }, deleteFile = FALSE)
+
 
 }
 
@@ -21,22 +41,50 @@ homeTab <- function(input, output, session,
 #'   This is useful when including this tab as an element of another tab.
 homeTabUI <- function(params, help = TRUE, ...) {
 
-    tabPanel(
+    mainPanel(
 
-        "Home Page", # text shown on the tab
+        #"Home Page", # text shown on the tab
 
-        h2("title at top of page"), # title at hop of page
+        imageOutput("mizer", width = "100%"),
 
-        p("first para goes here"),
+        # img(src ='EU.jpg', width = '100%', align = "center"),
+        # img(src = normalizePath(file.path('./www/EU.jpg')), width = '100%', align = "center"),
 
-        p("2nd para goes here"),
 
-        p("3rd para goes here"),
+        h2("Tuning and exploring multi-species size spectrum models parameters"), # title at hop of page
 
-        p("4th para goes here")#,
+        p("This application allows you to explore a wide range of outputs of multi-species size spectrum models
+          developed using the modelling package mizer (http://sizespectrum.org/mizer/). Mizer modelling framework
+          has been used to investigate a range of aquatics size-structured ecosystems. Its assumptions, equations,
+          user guides, examples and references are described in detail on http://sizespectrum.org/mizer/."),
 
-        #img(src ='logos.png', width = '100%', align = "center")
+        p("This shiny R application can be used for both exploring outputs of already existing models, but also
+          to tune parameters for models that are being developed. You can explore species and resource size spectra,
+          diets, biomasses and yields, sources of mortality across different sizes, growth and reproduction and so on.
+          For a quick set of instructions, click “Help” on the top left of this application. Mizer modelling community
+          has a range of online tutorials and running various online courses. "),
+
+        p("To run this application you will need a mizer add-on mizerMR, which enables to model ecosystems with multiple
+          size-structured background resources, such as plankton, benthos and others (e.g. different types of benthos).
+          You will also have to use the “experimental” mizer branch, which has a lot of new features but is not yet in
+          an official CRAN release."),
+
+        p("This shiny R application has been developed by Gustav Delius and Romain Forestier. Its development was
+          supported by the European Regional Development Fund (project No 01.2.2-LMT-K-718-02-0006) under grant
+          agreement with the Research Council of Lithuania (LMTLT)."),
+
+        # img(src ="IMAS.jpg", width = '25%', align = "left"),
+        # img(src ='mizer.png', width = '25%', align = "center"),
+        # img(src ='NatureResearchCentre.png', width = '25%', align = "center"),
+        # img(src ='SIF.jpg', width = '25%', align = "right"),
+
+
+        # imageOutput("imas",),
+        # imageOutput("eu"),
+        # imageOutput("nature"),
+        imageOutput("banner", width = "100%")
 
     )
 
 }
+
