72cc665c6a51c6dfd8ee57d29f0e830a76ab3dc1
Gustav Delius <gustav.delius@gmail.com>
2022-08-10 14:01:57 +0100

    The `match...()` functions now preserve reproduction level.

--- R/matchNumbers.R
+++ R/matchNumbers.R
@@ -45,6 +45,12 @@ matchNumbers <- function(params, species = NULL) {
     if (!("number_observed" %in% names(params@species_params))) {
         return(params)
     }
+    
+    # Preserve reproduction level if using BevertonHolt
+    if (params@rates_funcs$RDD == "BevertonHoltRDD") {
+        old_reproduction_level <- getReproductionLevel(params)
+    }
+    
     species <- valid_species_arg(params, species = species,
                                  return.logical = TRUE) &
         !is.na(params@species_params$number_observed) &
@@ -59,6 +65,11 @@ matchNumbers <- function(params, species = NULL) {
         factor <- params@species_params$number_observed[[sp]] / total
         params@initial_n[sp, ] <- params@initial_n[sp, ] * factor
     }
-
-    setBevertonHolt(params)
+    
+    # Preserve reproduction level if using BevertonHolt
+    if (params@rates_funcs$RDD == "BevertonHoltRDD") {
+        setBevertonHolt(params, reproduction_level = old_reproduction_level)
+    }
+    
+    params
 }
