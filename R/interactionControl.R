2d984291446a6fe968985645de4bb342d45daf87
Romain Forestier <Romain.Forestier@utas.edu.au>
2022-08-18 13:59:47 +1000

    adding MR backgrounds to interaction controls

--- R/interactionControl.R
+++ R/interactionControl.R
@@ -18,12 +18,27 @@ interactionControl <- function(input, output, session, params,
             flags$sp_old_inter <- sp
             return()
         }
+        
+        if(!is.null(getComponent(p, "MR"))){
+          res_df <- mizerMR::resource_interaction(p)
+          
+          for (iRes in 1:dim(res_df)[2]) {
+            res_var <- paste0("inter_res", iRes)
+            req(input[[inter_var]])
+            mizerMR::resource_interaction(p)[sp,iRes] <- input[[res_var]]
+          }
+          p@species_params[sp, "interaction_resource"] <-
+            input$interaction_resource
+        } else {
         p@species_params[sp, "interaction_resource"] <-
             input$interaction_resource
-        for (i in p@species_params$species[!is.na(p@A)]) {
-            inter_var <- paste0("inter_", i)
-            p@interaction[sp, i] <- input[[inter_var]]
         }
+        
+        for (i in p@species_params$species[!is.na(p@A)]) {
+          inter_var <- paste0("inter_", i)
+          p@interaction[sp, i] <- input[[inter_var]]
+        }
+
         tuneParams_update_species(sp, p, params, params_old)
     })
 }
@@ -34,12 +49,37 @@ interactionControl <- function(input, output, session, params,
 interactionControlUI <- function(p, input) {
     sp <- p@species_params[input$sp, ]
     l1 <- list(
-        tags$h3(tags$a(id = "interaction"), "Prey interactions"),
+        tags$h3(tags$a(id = "interaction"), "Prey interactions"))
+    
+        if(!is.null(getComponent(p, "MR"))){
+          res_df <- mizerMR::resource_interaction(p)
+          for(iRes in 1:dim(res_df)[2]){
+            res_var <- paste0("inter_res", iRes)
+            res_name <- colnames(res_df)[iRes]
+            l1 <- c(l1, list(
+          sliderInput(res_var, res_name,
+                      value = res_df[sp$species,iRes],
+                      min = 0,
+                      max = 1,
+                      step = 0.001)
+            ))
+          }
+          l1 <- c(l1, list(
+            sliderInput("interaction_resource", "Resource",
+                        value = sp$interaction_resource,
+                        min = 0,
+                        max = 1,
+                        step = 0.001)
+          ))
+        } else {
+          l1 <- c(l1, list(
         sliderInput("interaction_resource", "Resource",
                     value = sp$interaction_resource,
                     min = 0,
                     max = 1,
-                    step = 0.001))
+                    step = 0.001)
+          ))
+        }
     for (i in p@species_params$species[!is.na(p@A)]) {
         inter_var <- paste0("inter_", i)
         l1 <- c(l1, list(
