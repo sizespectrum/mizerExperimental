46fe8c086b8e9af61e4dda02b0cbcacffce4d6c5
Gustav Delius <gustav.delius@gmail.com>
2022-11-12 10:55:10 +0000

    New function `age_mat()` calculates age at maturity. `a_mat()` renamed to `age_mat_vB()`.

--- R/age_mat.R
+++ R/age_mat.R
@@ -1,4 +1,4 @@
-#' Calculate age at maturity for all species
+#' Calculate age at maturity from von Bertalanffy growth parameters
 #' 
 #' Uses the age at maturity that is implied by the von Bertalanffy growth
 #' curve specified by the `w_inf`, `k_vb`, `t0` parameters in the 
@@ -8,7 +8,7 @@
 #' @return A named vector. The names are the species names and the values are
 #'   the ages at maturity.
 #' @export
-a_mat <- function(params) {
+age_mat_vB <- function(params) {
     if (is(params, "MizerParams")) {
         sp <- params@species_params
     } else {
@@ -24,5 +24,35 @@ a_mat <- function(params) {
 
     a_mat <- -log(1 - (sp$w_mat / sp$w_inf) ^ (1/sp$b)) / sp$k_vb + sp$t0
     names(a_mat) <- sp$species
+    a_mat
+}
+
+#' Calculate age at maturity
+#' 
+#' Uses the growth rate and the size at maturity to calculate the age at 
+#' maturity
+#' 
+#' Using that by definition of the growth rate $g(w) = dw/dt$ we have that
+#' \deqn{\mathrm{age_{mat}} = \int_0^{w_{mat}.}\frac{dw}{g(w)}}{age_mat = \int_0^w_mat 1/g(w) dw.}
+#' 
+#' @param params A MizerParams object
+#' @return A named vector. The names are the species names and the values are
+#'   the ages at maturity.
+#' @export
+#' @examples 
+#' age_mat(NS_params)
+age_mat <- function(params) {
+    assert_that(is(params, "MizerParams"))
+    sp <- params@species_params
+    no_sp <- nrow(sp)
+    
+    g <- getEGrowth(params)
+    a_mat <- vector("double", no_sp)
+    names(a_mat) <- sp$species
+    for (i in seq_along(a_mat)) {
+        sel <- params@w < sp$w_mat[i]
+        a_mat[i] <- sum(params@dw[sel] / g[i, sel])
+    }
+    
     a_mat
 }
