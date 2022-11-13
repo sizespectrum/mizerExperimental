69af67048b57e239cbc4b7b17415a7e9b282f753
Gustav Delius <gustav.delius@gmail.com>
2022-11-13 20:05:37 +0000

    `get_h_default()` now uses `age_mat`
    Should be moved back to mizer for the next release.

--- R/age_mat.R
+++ R/age_mat.R
@@ -4,25 +4,25 @@
 #' specified by the `w_inf`, `k_vb`, `t0`, `a` and `b` parameters in the
 #' species_params data frame.
 #'
-#' If any of `a`, `b` or `k_vb` is missing for a species, the function returns
-#' NA for that species. If `t0` is missing a default of 0 is used.
+#' If any of `k_vb` is missing for a species, the function returns NA for that
+#' species. Default values of `b = 3` and `t0 = 0` are used if these are
+#' missing.
 #'
 #' @param params A MizerParams object or a species_params data frame
 #' @return A named vector. The names are the species names and the values are
 #'   the ages at maturity.
 #' @export
-age_mat_vB <- function(params) {
-    if (is(params, "MizerParams")) {
-        sp <- params@species_params
+age_mat_vB <- function(object) {
+    if (is(object, "MizerParams")) {
+        sp <- object@species_params
     } else {
-        if (!is.data.frame(params)) {
+        if (!is.data.frame(object)) {
             stop("The first argument must be either a MizerParams object or a species_params data frame.")
         }
-        sp <- validSpeciesParams(params)
+        sp <- validSpeciesParams(object)
     }
     sp <- set_species_param_default(sp, "t0", 0)
-    sp <- set_species_param_default(sp, "a", NA)
-    sp <- set_species_param_default(sp, "b", NA)
+    sp <- set_species_param_default(sp, "b", 3)
     sp <- set_species_param_default(sp, "k_vb", NA)
 
     a_mat <- -log(1 - (sp$w_mat / sp$w_inf) ^ (1/sp$b)) / sp$k_vb + sp$t0
