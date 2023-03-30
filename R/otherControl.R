21502c6650d46959c50f0fb2c221dd3cacd99371
Gustav Delius <gustav.delius@gmail.com>
2023-03-30 13:07:11 +0100

    Allow `otherControl()` to change external mortality even if it is protected.

--- R/otherControl.R
+++ R/otherControl.R
@@ -1,5 +1,21 @@
 #' Controlling the species parameters not included in other controls
 #' @inheritParams abundanceControl
+#'
+#'   Adds controls for the parameters that did not fit into other categories.
+#'
+#'   The parameters included are:
+#' *  conversion efficiency `alpha`
+#' *  metabolic rate coefficient `ks` and exponent `p`
+#' *  activity rate coefficient `k`
+#' *  external mortality `z0`.
+#'
+#'   If the external mortality `z0` is changed, then the corresponding rate
+#'   array is changed by the difference between the new value and the previous
+#'   value. Thus any additions made to the rate by the user independently of the
+#'   species parameters are preserved.
+#'
+#'   If the metabolism exponent `p` is changed, this also changes `ks` so
+#'   that metabolic rate at maturity stays the same.
 otherControl <- function(input, output, session, params, params_old,
                          flags, ...) {
     observe({
@@ -18,16 +34,20 @@ otherControl <- function(input, output, session, params, params_old,
         updateSliderInput(session, "k",
                           min = signif(input$k / 2, 2),
                           max = signif((input$k + 0.1) * 1.5, 2))
-        updateSliderInput(session, "z0",
-                          min = signif(input$z0 / 2, 2),
-                          max = signif((input$z0 + 0.1) * 1.5, 2))
+        
+        if (p@species_params[sp, "z0"] != input$z0) {
+            updateSliderInput(session, "z0",
+                              min = signif(input$z0 / 2, 2),
+                              max = signif((input$z0 + 0.1) * 1.5, 2))
+            # re-calculate ext_mort
+            ext_mort(p)[sp, ] <- ext_mort(p)[sp, ] -
+                (input$z0 - p@species_params[sp, "z0"])
+        }
 
         p@species_params[sp, "alpha"] <- input$alpha
         p@species_params[sp, "ks"]    <- input$ks
         p@species_params[sp, "k"]     <- input$k
-        p@species_params[sp, "z0"]    <- input$z0
         p <- setMetabolicRate(p)
-        p <- setExtMort(p)
         tuneParams_update_species(sp, p, params, params_old)
     })
 
@@ -44,7 +64,7 @@ otherControl <- function(input, output, session, params, params_old,
             p@species_params[[sp, "p"]] <- input$p
             ks <- p@species_params[[sp, "ks"]]
             updateSliderInput(session, "ks",
-                              value = ks,
+                              value = ks, # this will trigger the other observer
                               min = signif(ks / 2, 2),
                               max = signif((ks + 0.1) * 1.5, 2))
             updateSliderInput(session, "p",
