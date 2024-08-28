d8d52a052183b2a14d552e14e6346191d4ad244c
Gustav Delius <gustav.delius@gmail.com>
2024-08-28 15:09:36 +0100

    Avoid warning in test

--- tests/testthat/test-matchYield.R
+++ tests/testthat/test-matchYield.R
@@ -3,13 +3,14 @@ test_that("matchYield works", {
     initial_effort(params) <- 1
     yields <- getYieldGear(params)
     gp <- params@gear_params
-    # Add yield_observed column but make it twice too large
+    # Add yield_observed column but make it twice too small
     for (i in seq_len(nrow(gp))) {
         species <- as.character(gp$species[i])
         gear <- as.character(gp$gear[i])
-        gp$yield_observed[i] <- yields[gear, species] * 2
+        gp$yield_observed[i] <- yields[gear, species] / 2
     }
     gear_params(params) <- gp
     p <- matchYield(params)
-    expect_equal(getYieldGear(params) * 2, getYieldGear(p))
+    expect_equal(p@gear_params$catchability,
+                 params@gear_params$catchability / 2)
 })
