be94a67f1b3c127fde5cea4455ec4e6cf1086665
Gustav Delius <gustav.delius@gmail.com>
2025-07-10 15:29:49 +0100

    `matchYield()` now has `species` and `gears` arguments to update only selected catchabilities.

--- R/matchYield.R
+++ R/matchYield.R
@@ -5,45 +5,50 @@
 #' yield and observed yield. 
 #' 
 #' @param params A MizerParams object
+#' @param species The species to be affected. Optional. By default all observed
+#'   biomasses will be matched. A vector of species names, or a numeric vector
+#'   with the species indices, or a logical vector indicating for each species
+#'   whether it is to be affected (TRUE) or not.
+#' @param gears The gears to be affected. Optional. By default all gears will be
+#'   affected. A vector of gear names.
 #' @param keep A string determining which quantity is to be kept constant. The
 #'   choices are "egg" which keeps the egg density constant, "biomass" which 
 #'   keeps the total biomass of the species constant and "number" which keeps
 #'   the total number of individuals constant.
 #' @return A MizerParams object with updated catchabilities
 #' @export
-matchYield <- function(params,
+matchYield <- function(params, species = NULL, gears = NULL,
                        keep = c("egg", "biomass", "number")) {
     assert_that(is(params, "MizerParams"))
     keep <- match.arg(keep)
-    
+    species_selected <- valid_species_arg(params, species = species)
+    gears_selected <- valid_gears_arg(params, gears = gears)
     biomass <- getBiomass(params)
     number <- getN(params)
     
     gp <- params@gear_params
+    gp_sel <- gp$species %in% species_selected & 
+        gp$gear %in% gears_selected &
+        !is.na(gp$yield_observed)
+    gp <- gp[gp_sel, ]
     if (is.null(gp$yield_observed)) {
         stop("You need to specify `yield_observed` in the gear parameter dataframe.")
     }
-    gp <- gp[!is.na(gp$yield_observed), ]
     yields <- getYieldGear(params)
     for (i in seq_len(nrow(gp))) {
         gear <- as.character(gp$gear[i])
         species <- as.character(gp$species[i])
         ratio <- gp$yield_observed[i] / yields[gear, species]
         if (!is.nan(ratio)) {
-            gear_params(params)[i, "catchability"] <- gp$catchability[i] * ratio
+            gp[i, "catchability"] <- gp$catchability[i] * ratio
         }
     }
+    gear_params(params)[gp_sel, ] <- gp
     
-    params <- steadySingleSpecies(params)
+    params <- steadySingleSpecies(params, species = species_selected,
+                                  keep = keep)
     
-    if (keep == "biomass") {
-        factor <- biomass / getBiomass(params)
-        params@initial_n <- params@initial_n * factor
-    }
-    if (keep == "number") {
-        factor <- number / getN(params)
-        params@initial_n <- params@initial_n * factor
-    }
-    
-    setBevertonHolt(params)
+    erepro <- params@species_params[species_selected, "erepro"]
+    names(erepro) <- species_selected
+    setBevertonHolt(params, erepro = erepro)
 }
--- tests/testthat/test-matchYield.R
+++ tests/testthat/test-matchYield.R
@@ -10,6 +10,17 @@ test_that("matchYield works", {
         gp$yield_observed[i] <- yields[gear, species] / 2
     }
     gear_params(params) <- gp
+    
+    # Change only some catchabilities
+    p <- matchYield(params, species = c("Dab", "Sole"), gears = "Beam")
+    expect_equal(p@gear_params["Dab, Beam", "catchability"],
+                 params@gear_params["Dab, Beam", "catchability"] / 2)
+    expect_equal(p@gear_params["Sole, Beam", "catchability"],
+                 params@gear_params["Sole, Beam", "catchability"] / 2)
+    expect_equal(p@gear_params["Plaice, Beam", "catchability"],
+                 params@gear_params["Plaice, Beam", "catchability"])
+    
+    # Change all catchabilities
     p <- matchYield(params)
     expect_equal(p@gear_params$catchability,
                  params@gear_params$catchability / 2)
