eef632aeaeb8f06d040dd9a1bcc290de5017f82d
Gustav Delius <gustav.delius@gmail.com>
2026-06-09 16:00:40 +0100

    Deprecated `plotBiomassRelative` and `plotYieldRelative`.

--- R/plotRelative.R
+++ R/plotRelative.R
@@ -1,5 +1,13 @@
 #' Plot change in biomass over time
 #'
+#' `r lifecycle::badge("deprecated")` A similar plot can now be produced with
+#' mizer's [mizer::plotRelative()], which works directly on the arrays returned
+#' by [mizer::getBiomass()], for example
+#' `plotRelative(getBiomass(sim_original), getBiomass(sim))`. Note that
+#' [mizer::plotRelative()] shows the symmetric relative difference
+#' \eqn{2(N_2 - N_1)/(N_1 + N_2)} rather than the percentage change relative to
+#' the reference.
+#'
 #' @param sim A MizerSim object
 #' @param sim_original Another MizerSim object to compare the biomasses to. If
 #'   NULL (default) then the biomasses are compared to the initial biomasses in
@@ -14,6 +22,10 @@
 #' plotBiomassRelative(NS_sim)
 #' plotBiomassRelative(NS_sim, species = c("Cod", "Sole"))
 plotBiomassRelative <- function(sim, sim_original = NULL, species = NULL, ...) {
+    lifecycle::deprecate_soft(
+        "3.0.0", "plotBiomassRelative()", "mizer::plotRelative()",
+        details = "For example `plotRelative(getBiomass(sim_original), getBiomass(sim))`."
+    )
     sel <- valid_species_arg(sim, species, return.logical = TRUE)
     if (is.null(sim_original)) {
         biomass_original <- getBiomass(sim@params, ...)[sel]
@@ -30,11 +42,23 @@ plotBiomassRelative <- function(sim, sim_original = NULL, species = NULL, ...) {
 #' @rdname plotBiomassRelative
 #' @export
 plotlyBiomassRelative <- function(sim, sim_original = NULL, species = NULL, ...) {
+    lifecycle::deprecate_soft(
+        "3.0.0", "plotlyBiomassRelative()", "mizer::plotRelative()",
+        details = "For example `plotRelative(getBiomass(sim_original), getBiomass(sim))`."
+    )
     ggplotly(plotBiomassRelative(sim, sim_original, species = species, ...),
              tooltip = c("Species", "Year", "Change %"))
 }
 
 #' Plot change in yield over time
+#'
+#' `r lifecycle::badge("deprecated")` A similar plot can now be produced with
+#' mizer's [mizer::plotRelative()], which works directly on the arrays returned
+#' by [mizer::getYield()], for example
+#' `plotRelative(getYield(object_original), getYield(sim))`. Note that
+#' [mizer::plotRelative()] shows the symmetric relative difference
+#' \eqn{2(N_2 - N_1)/(N_1 + N_2)} rather than the percentage change relative to
+#' the reference.
 #'
 #' @param sim A MizerSim object
 #' @param object_original A MizerParams or MizerSim object to calculate
@@ -49,6 +73,10 @@ plotlyBiomassRelative <- function(sim, sim_original = NULL, species = NULL, ...)
 #' plotYieldRelative(NS_sim, NS_params)
 #' plotYieldRelative(NS_sim, NS_params, species = c("Cod", "Sole"))
 plotYieldRelative <- function(sim, object_original, species = NULL, ...) {
+    lifecycle::deprecate_soft(
+        "3.0.0", "plotYieldRelative()", "mizer::plotRelative()",
+        details = "For example `plotRelative(getYield(object_original), getYield(sim))`."
+    )
     sel <- valid_species_arg(sim, species, return.logical = TRUE)
     if (is(object_original, "MizerParams")) {
         yield_original <- getYield(object_original, ...)[sel]
@@ -65,6 +93,10 @@ plotYieldRelative <- function(sim, object_original, species = NULL, ...) {
 #' @rdname plotYieldRelative
 #' @export
 plotlyYieldRelative <- function(sim, object_original, species = NULL, ...) {
+    lifecycle::deprecate_soft(
+        "3.0.0", "plotlyYieldRelative()", "mizer::plotRelative()",
+        details = "For example `plotRelative(getYield(object_original), getYield(sim))`."
+    )
     ggplotly(plotYieldRelative(sim, object_original, species = species, ...),
              tooltip = c("Species", "Year", "Change %"))
 }
